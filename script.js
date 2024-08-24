b3abd638d845fa1435c0a0149860867793cce9e8
abhisingh3960 <94777478+abhisingh3960@users.noreply.github.com>
2024-08-24 22:07:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 function insert_Row() {
-    // Get the table element by its ID
+   
     let table = document.getElementById('sampleTable');
 
-    // Insert a new row at the end of the table
-    let Row = table.insertRow(-1);
+    let Row = table.insertRow(0);
 
-    // Get the current number of columns in the first row
-    let colCount = table.rows[0].cells.length;
+    // Insert new cells in the new row (2 cells)
+    let cell1 = Row.insertCell(0);
+    let cell2 = Row.insertCell(1);
 
-    // Insert cells into the new row, adding one more column than the previous rows
-    for (let i = 0; i <= colCount; i++) {
-        let cell = Row.insertCell(i);
-        cell.innerHTML = `Row${table.rows.length} cell${i + 1}`;
-    }
+    
+    cell1.innerHTML = "New cell1";
+    cell2.innerHTML = "New cell2";
 }
