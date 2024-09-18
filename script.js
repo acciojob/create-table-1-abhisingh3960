dd51590d9d9a724baad5eec669c86386449a8b77
abhisingh3960 <94777478+abhisingh3960@users.noreply.github.com>
2024-09-18 21:34:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 function insert_Row() {
-   
     let table = document.getElementById('sampleTable');
-
     let Row = table.insertRow(0);
-
-    // Insert new cells in the new row (2 cells)
     let cell1 = Row.insertCell(0);
-    let cell2 = Row.insertCell(1);
-
-    
+    let cell2 = Row.insertCell(1); 
     cell1.innerHTML = "New Cell1";
     cell2.innerHTML = "New Cell2";
 }
